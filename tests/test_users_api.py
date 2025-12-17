8507849bd8830a5713213ef9d38c710a8473dd73
Dewayne Allen <dewayneallen76@gmail.com>
2025-12-17 17:32:58 -0500

    Added constant for url and a test for user endpoint

--- tests/test_users_api.py
+++ tests/test_users_api.py
@@ -1,5 +1,24 @@
 import requests
 
+# Contstant for API url
+API_URL = "https://jsonplaceholder.typicode.com"
+
+# Simple test to confirm that API can be reached 
 def test_api_is_reachable():
-    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    response = requests.get(API_URL)
     assert response.status_code == 200
+
+# Test to get user and confirm data 
+def test_get_user():
+    endpoint = f"{API_URL}/users/1"
+    response = requests.get(endpoint)
+
+    # Confirm that endpoint can be reached
+    assert response.status_code == 200
+    
+    # Parse the JSON data
+    data = response.json()
+    
+    # Check that data is present
+    assert data['id'] == 1
+    assert data['name'] == "Leanne Graham"
